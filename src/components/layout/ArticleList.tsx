d92531635fd1e07b53f9ad5885aa61b501b95a57
Zsolt <zsolt@wavedropper.com>
2026-02-24 15:28:13 +0000

    Improve article list's style

--- src/components/layout/ArticleList.tsx
+++ src/components/layout/ArticleList.tsx
@@ -21,7 +21,7 @@ export function ArticleList({
   console.log(disableListControls);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 items-end">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 max-w-400 items-end mx-auto">
       {articles.map((article) => {
         return (
           <ListItem
--- src/components/layout/ListItem.tsx
+++ src/components/layout/ListItem.tsx
@@ -1,8 +1,9 @@
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { preload } from "react-dom";
+import { Link } from "react-router";
 import type { Article } from "../../lib/types";
 import { formatDate } from "../../lib/utils";
-import { ThumbsUp } from "lucide-react";
+import { ThumbsUp, Eye } from "lucide-react";
 import "react-lazy-load-image-component/src/effects/blur.css";
 
 export function ListItem({
@@ -23,9 +24,14 @@ export function ListItem({
       className="article-card m-6 flex flex-col justify-between gap-1"
     >
       {!loading ?
-        <h3 className="font-bold text-xl h-full text-c-jetblack">
-          {article?.title}
-        </h3>
+        <Link
+          className="group relative block"
+          to={"/articles/" + article?.article_id}
+        >
+          <h3 className="font-bold text-xl h-full text-c-jetblack">
+            {article?.title}
+          </h3>
+        </Link>
       : <div className="flex flex-col gap-1">
           <div className="h-6.5 bg-c-powderblue rounded-2xl animate-pulse" />
           <div className="h-6.5 bg-c-powderblue rounded-2xl animate-pulse" />
@@ -34,16 +40,21 @@ export function ListItem({
 
       <p className="flex justify-between"></p>
       {!loading ?
-        <div className={`pb-2 bg-c-burntpeach text-right rounded-xl`}>
-          <LazyLoadImage
-            className="self-end h-52 w-full rounded-t-xl mb-1"
-            src={article?.article_img_url}
-            effect="blur"
-            wrapperProps={{
-              // If you need to, you can tweak the effect transition using the wrapper style.
-              style: { transitionDelay: "1s" },
-            }}
-          />
+        <div className={`relative pb-2 bg-c-burntpeach text-right rounded-xl`}>
+          <Link
+            className="group relative block"
+            to={"/articles/" + article?.article_id}
+          >
+            <LazyLoadImage
+              className="self-end h-52 w-full rounded-t-xl mb-1 group-hover:brightness-120 transition-all"
+              src={article?.article_img_url}
+              effect="blur"
+              wrapperProps={{
+                style: { transitionDelay: "1s" },
+              }}
+            />
+            <Eye className="opacity-0 group-hover:opacity-100 transition-all absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 size-32 text-c-powderblue/80 backdrop-blur-sm rounded-full" />
+          </Link>
           <div className="flex justify-between px-3">
             <span className="text-white font-semibold">
               {formatDate(article?.created_at)}
