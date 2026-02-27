3bc78d30c8dea4a3d396764b58fd64939ace7955
Zsolt <zsolt@wavedropper.com>
2026-02-27 10:54:59 +0000

    Update error display component to improve visibility

--- src/components/ErrorDisplay.tsx
+++ src/components/ErrorDisplay.tsx
@@ -2,7 +2,7 @@ export default function ErrorDisplay({ error }: { error?: Error }) {
   if (!error?.message) return;
 
   return (
-    <div className="absolute right-1/2 translate-x-1/2 bg-c-burntpeach/80 backdrop-blur-sm text-white p-3 rounded-xl">
+    <div className="fixed z-50 animate-bounce right-1/2 translate-x-1/2 bg-c-burntpeach/80 backdrop-blur-sm text-white p-3 rounded-xl">
       {error?.message && <p>{error?.message}</p>}
     </div>
   );
