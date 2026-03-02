10820541a16f7b9ad7e14bced2cfb504045651f3
Zsolt <zsolt@wavedropper.com>
2026-03-02 22:13:31 +0000

    Make header icon to navigate to homepage

--- src/components/Branding.tsx
+++ src/components/Branding.tsx
@@ -1,15 +1,16 @@
+import { Link } from "react-router";
 import { Newspaper } from "lucide-react";
 
 export function Branding() {
   return (
-    <div className="p-3 flex flex-col items-center">
+    <Link to="/" className="p-3 flex flex-col items-center">
       <Newspaper className="size-12 sm:size-16 md:size-22" />
       <p className="font-mono">
         <span className="text-white bg-c-jetblack rounded-xs px-2">
           NC_NEWS
         </span>
       </p>
-    </div>
+    </Link>
   );
 }
 
