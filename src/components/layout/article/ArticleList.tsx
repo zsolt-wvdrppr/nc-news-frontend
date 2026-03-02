073ff2d138da6f96240e20a45b08514ce0af3b9f
Zsolt <zsolt@wavedropper.com>
2026-03-02 18:06:33 +0000

    Improve topic dropdown selector's design

--- src/components/layout/article/ArticleList.tsx
+++ src/components/layout/article/ArticleList.tsx
@@ -37,7 +37,7 @@ export function ArticleList({
   }, [filter]);
 
   return (
-    <div>
+    <div className="max-w-400 mx-auto">
       {enableListControls && <ListControls />}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-10 max-w-400 items-end mx-auto p-3">
         {!loading &&
--- src/components/layout/article/ListControls.tsx
+++ src/components/layout/article/ListControls.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import { useContent } from "../../../lib/hooks/useContent";
 import { fetchContent } from "../../../lib/api";
 import type { FilterType } from "../../../lib/types";
 import { useNavigate } from "react-router";
+import { Minus, Plus, Trash2 } from "lucide-react";
 
 export function ListControls({}: {}) {
   const { content } = useContent(fetchContent, {
@@ -9,41 +11,77 @@ export function ListControls({}: {}) {
     expectedType: "topic-list",
   });
 
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const handleDropDownBtn = () => {
+    setIsOpen(!isOpen);
+  };
+
   const navigate = useNavigate();
 
   console.log(content);
 
   const updateFilter = (filter: FilterType) => {
     console.log("update filter");
-    if (filter?.topic) navigate("/archive/topics/" + filter.topic);
+    if (filter?.topic) {
+      navigate("/archive/topics/" + filter.topic);
+    } else {
+      navigate("/archive/");
+    }
   };
 
   const handleTopicUpdate = (value: string) => {
-    console.log(value);
     updateFilter({ topic: value });
   };
 
   if (content?.type === "topic-list")
     return (
-      <div className="w-fit min-w-96 mx-auto mb-12">
-        <select
-          className="border-3 border-c-burntpeach w-full h-10 font-semibold text-lg outline-none rounded-2xl"
-          onChange={(e) => {
-            handleTopicUpdate(e.target.value);
-          }}
+      <div className="topic-selector-container w-fit min-w-60 mb-12">
+        <div
+          id="topic-selector"
+          className={`relative font-semibold text-lg outline-none rounded-xl flex flex-col`}
         >
-          <option key="default-key" className="text-center" defaultChecked>
-            Select Topic
-          </option>
-          {content.type === "topic-list" &&
-            content.topics.map((topic) => {
-              return (
-                <option key={topic.slug} className="" value={topic.slug}>
-                  {topic.slug}
-                </option>
-              );
-            })}
-        </select>
+          <button
+            key="default-key"
+            className={`${isOpen ? "rounded-t-xl" : "rounded-xl"} z-20 bg-c-duskblue pb-2 transition-all duration-50 relative text-center text-white font-semibold flex flex-row items-center justify-center px-2`}
+            onClick={handleDropDownBtn}
+          >
+            <span className="pt-1">{"- Select Topic -"}</span>
+            {isOpen && (
+              <Minus className="absolute right-0 stroke-white mt-2 mr-1" />
+            )}
+            {!isOpen && (
+              <Plus className="absolute right-0 stroke-white mt-2 mr-1" />
+            )}
+          </button>
+          <div
+            className={`${isOpen ? "opacity-100 z-10" : "opacity-0 -translate-y-8 -z-10"} absolute w-full top-7.5 transition-all duration-500 topic-list-wrapper pb-2 pt-3 flex flex-col gap-3 backdrop-blur-sm bg-c-duskblue/90`}
+          >
+            {content.type === "topic-list" &&
+              content.topics.map((topic) => {
+                return (
+                  <button
+                    key={topic.slug}
+                    className="text-center hover:bg-c-jetblack/50 py-2 text-white"
+                    onClick={() => {
+                      handleTopicUpdate(topic.slug);
+                    }}
+                  >
+                    {topic.slug.slice(0, 1).toUpperCase() + topic.slug.slice(1)}
+                  </button>
+                );
+              })}
+            <button
+              key={"reset"}
+              className="text-center hover:bg-c-burntpeach/50 py-2 text-white flex flex-row justify-center"
+              onClick={() => {
+                handleTopicUpdate("");
+              }}
+            >
+              <Trash2 className="absolute left-5 bottom-4" /> {"- Reset -"}
+            </button>
+          </div>
+        </div>
       </div>
     );
 }
