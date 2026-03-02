1345e8b2e87e0a8c9996453c0e7f74b47789007c
Zsolt <164944009+zsolt-wvdrppr@users.noreply.github.com>
2026-03-02 23:39:04 +0000

    Update ArticleList.tsx padding

--- src/components/layout/article/ArticleList.tsx
+++ src/components/layout/article/ArticleList.tsx
@@ -47,7 +47,7 @@ export function ArticleList({
   }, [filter, searchParams]);
 
   return (
-    <div className="max-w-400 mx-auto">
+    <div className="max-w-400 mx-auto p-3">
       {enableListControls && <ListControls />}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-x-6 gap-y-10 max-w-400 items-end mx-auto p-3">
         {!loading &&
