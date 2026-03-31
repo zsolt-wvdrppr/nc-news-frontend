17807a4feacb7c39277bd14a25fbf0bb53060e90
Zsolt <zsolt@wavedropper.com>
2026-03-31 20:54:03 +0100

    Utilise useLimitVote hook and update VoteBar accordingly

--- src/components/layout/VoteBar.tsx
+++ src/components/layout/VoteBar.tsx
@@ -1,6 +1,7 @@
 import { ThumbsUp } from "lucide-react";
 import { useState, type ComponentPropsWithoutRef } from "react";
 import { useContent } from "../../lib/hooks/useContent";
+import { useLimitVote } from "../../lib/hooks/useLimitVote";
 import { fetchContent } from "../../lib/api";
 import type { Options } from "../../lib/types";
 
@@ -15,33 +16,50 @@ export function VoteBar({
   const [votes, setVotes] = useState(initialVotes);
 
   const { setTrigger } = useContent(fetchContent, options, "trigger");
+  const { hasVote, storeVote } = useLimitVote();
+
+  const articleId = Number(options.articleId);
+  const hasVoted = hasVote(articleId);
 
   options.method = "PATCH";
   options.expectedType = "comment";
 
   const handleUpVote = () => {
-    options.body = { inc_votes: 1 };
-
-    setTrigger(true);
-    setVotes(votes + 1);
+    if (hasVoted === 1) {
+      handleDownVote();
+      storeVote({ vote: 0, articleId: articleId });
+    } else {
+      options.body = { inc_votes: 1 };
+      storeVote({ vote: 1, articleId: articleId });
+      setTrigger(true);
+      setVotes(votes + 1);
+    }
   };
 
   const handleDownVote = () => {
-    options.body = { inc_votes: -1 };
-    setTrigger(true);
-    setVotes(votes - 1);
+    if (hasVoted === -1) {
+      handleUpVote();
+      storeVote({ vote: 0, articleId: articleId });
+    } else {
+      options.body = { inc_votes: -1 };
+      storeVote({ vote: -1, articleId: Number(options.articleId) });
+      setTrigger(true);
+      setVotes(votes - 1);
+    }
   };
 
   return (
     <div {...props}>
       <button aria-label="down-vote" onClick={handleDownVote}>
-        <ThumbsUp className="rotate-180 active:scale-150 active:opacity-50 hover:scale-125 hover:stroke-1.5 transition-all cursor-pointer" />
+        <ThumbsUp
+          className={`rotate-180 active:scale-150 active:opacity-50 hover:scale-125 hover:stroke-1.5 transition-all cursor-pointer ${hasVote(articleId) === -1 ? "fill-white/60" : ""}`}
+        />
       </button>
       <span className=" font-bold">{votes}</span>
       <button>
         <ThumbsUp
           aria-label="up-vote"
-          className="hover:scale-125 active:scale-150 active:opacity-50 transition-all hover:stroke-1.5 cursor-pointer"
+          className={`hover:scale-125 active:scale-150 active:opacity-50 transition-all hover:stroke-1.5 cursor-pointer  ${hasVote(articleId) === 1 ? "fill-white/60" : ""}`}
           onClick={handleUpVote}
         />
       </button>
